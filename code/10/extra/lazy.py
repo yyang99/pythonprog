3af973e889ceeb754b0dd25e925eb6c44b986ffa
Yufeng Yang <yufeng.yang@bell.ca>
2022-01-16 22:32:19 -0500

    ch10 lazy

---
+++ code/10/extra/lazy.py
@@ -0,0 +1,20 @@
+class Lazy:
+    def __init__(self, func):
+        self.func = func
+    def __set_name__(self, owner, name):
+        self.key = name
+    def __get__(self, instance, owner):
+        if instance:
+            value = self.func(instance)
+            instance.__dict__[self.key] = value
+            return value
+        else:
+            return self
+
+class Rectangle:
+    area = Lazy(lambda self: self.length * self.height)
+    perimeter = Lazy(lambda self: 2 * self.height + 2 * self.width)
+    def __init__(self, width, height):
+        self.length = width
+        self.height = height
+    
